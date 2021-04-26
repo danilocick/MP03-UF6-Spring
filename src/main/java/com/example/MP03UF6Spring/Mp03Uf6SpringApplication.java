f249a46aadf02a86fe5e63948e87905f3d081c6b
danilocick <dhernandez@elpuig.xeill.net>
2021-04-26 18:36:56 +0200

    Initial commit

---
+++ src/main/java/com/example/MP03UF6Spring/Mp03Uf6SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.MP03UF6Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp03Uf6SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp03Uf6SpringApplication.class, args);
+	}
+
+}
